67966c89e859e4612a28e06f994953f10a0f5883
Wasse Lucas <wasselucas.silva@gmail.com>
2023-11-13 17:39:47 -0300

    criando esqueleto da requisicao get buscar todos os videos

---
+++ src/main/java/com/alura/api/controller/VideosContoller.java
@@ -0,0 +1,15 @@
+package com.alura.api.controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
+public class VideosContoller {
+
+    @GetMapping("/videos")
+    public String  getAll(){
+        return "Aqui exibe todos os videos";
+    }
+    
+}
