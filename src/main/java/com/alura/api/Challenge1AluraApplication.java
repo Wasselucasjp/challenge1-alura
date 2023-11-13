d2b2e82c79d50b593d49f34c639c6d81c1d92341
Wasse Lucas <wasselucas.silva@gmail.com>
2023-11-13 17:17:52 -0300

    primeiro commit

---
+++ src/main/java/com/alura/api/Challenge1AluraApplication.java
@@ -0,0 +1,13 @@
+package com.alura.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Challenge1AluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Challenge1AluraApplication.class, args);
+	}
+
+}
