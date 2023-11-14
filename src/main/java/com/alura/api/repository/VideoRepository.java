80f3813fd94459baf6d06fa8fe375b30f473bfd8
Wasse Lucas <wasselucas.silva@gmail.com>
2023-11-14 16:43:46 -0300

    adicionando anotacao repository

--- src/main/java/com/alura/api/repository/VideoRepository.java
+++ src/main/java/com/alura/api/repository/VideoRepository.java
@@ -1,7 +1,9 @@
 package com.alura.api.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.alura.api.model.Video;
 
+@Repository
 public interface VideoRepository extends JpaRepository<Video, Long> {}
